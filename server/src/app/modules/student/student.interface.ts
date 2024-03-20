d34655522374015ed73173e010e3c86f6ec0cfcc
User <tamim200091@gmail.com>
2024-03-21 03:00:46 +0600

    ER Diagram & Requirement Analysis Document updated

--- server/src/app/modules/student/student.interface.ts
+++ server/src/app/modules/student/student.interface.ts
@@ -6,6 +6,16 @@ export type TName = {
   lastName: string;
 };
 
+export type TBloodGroup =
+  | 'A+'
+  | 'A-'
+  | 'B+'
+  | 'B-'
+  | 'AB+'
+  | 'AB-'
+  | 'O+'
+  | 'O-';
+
 export type TIndividualGuardian = {
   name: TName;
   occupation: string;
@@ -22,24 +32,79 @@ export type TLocalGuardian = TIndividualGuardian & {
   relationship: string;
 };
 
+export type TAcademicDepartment =
+  | 'Computer Science'
+  | 'Electrical Engineering'
+  | 'Mechanical Engineering'
+  | 'Civil Engineering'
+  | 'Biology'
+  | 'Chemistry'
+  | 'Physics'
+  | 'Mathematics'
+  | 'Business Administration'
+  | 'Economics'
+  | 'Psychology'
+  | 'Sociology'
+  | 'English Literature'
+  | 'History'
+  | 'Political Science'
+  | 'Environmental Science'
+  | 'Fine Arts'
+  | 'Music'
+  | 'Health Sciences'
+  | 'Linguistics';
+
+export type TNationality =
+  | 'American'
+  | 'British'
+  | 'Canadian'
+  | 'Chinese'
+  | 'French'
+  | 'German'
+  | 'Indian'
+  | 'Italian'
+  | 'Japanese'
+  | 'Russian'
+  | 'Spanish'
+  | 'Swiss'
+  | 'Australian'
+  | 'Brazilian'
+  | 'Mexican'
+  | 'South Korean'
+  | 'Turkish';
+
+export type TReligion =
+  | 'Christianity'
+  | 'Islam'
+  | 'Hinduism'
+  | 'Buddhism'
+  | 'Judaism'
+  | 'Sikhism'
+  | "Bahá'í Faith"
+  | 'Jainism'
+  | 'Shinto'
+  | 'Taoism'
+  | 'Zoroastrianism';
+
 export type TStudent = {
   id: string;
   user: Types.ObjectId;
-  password: string;
   name: TName;
   gender: 'male' | 'female' | 'others';
   dateOfBirth: Date;
   email: string;
   contactNo: string;
   emergencyContactNo: string;
-  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  bloodGroup?: TBloodGroup;
   presentAddress: string;
   permanentAddress: string;
   guardian: TGuardian;
   localGuardian: TLocalGuardian;
-  profileImg?: string;
+  profileImage?: string;
   admissionSemester: Types.ObjectId;
-  isDeleted: boolean;
+  academicDepartment: TAcademicDepartment;
+  nationality: TNationality;
+  religion: TReligion;
 };
 
 //* For creating static
